26c8e756e34dca8e827a24bcdd0e584f10a56eac
Toido <caguiadaryl@gmail.com>
2024-01-25 19:30:53 +0800

    Apply routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './views/Home/Home';
+import Details from './views/Details/Details';
+import AppProvider from './providers/AppProvider';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <AppProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:catId" element={<Details />} />
+          <Route path="/?breed=:breedId" element={<Home />} />
+        </Routes>
+      </AppProvider>
     </BrowserRouter>
   );
 }
