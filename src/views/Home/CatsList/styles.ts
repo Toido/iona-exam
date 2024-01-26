2f08ccf358c6603f264b704d900c6e262fbbcfd9
Toido <caguiadaryl@gmail.com>
2024-01-26 23:09:35 +0800

    Add styles for component

--- src/views/Home/CatsList/styles.ts
+++ src/views/Home/CatsList/styles.ts
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { Button, ButtonProps, Card } from 'react-bootstrap';
+import { Button, ButtonProps, Card, Col } from 'react-bootstrap';
 import styled from 'styled-components';
 
 export const StyledCard = styled(Card)`
@@ -10,4 +10,8 @@ export const StyledButton = styled(Button as FC<ButtonProps>)`
   color: #fff;
   background-color: #28a745;
   border-color: #28a745;
+`;
+
+export const StyledCol = styled(Col)`
+  margin-bottom: 20px;
 `;
---
+++ src/views/Home/styles.ts
@@ -0,0 +1,6 @@
+import { Row } from 'react-bootstrap';
+import styled from 'styled-components';
+
+export const StyledRow = styled(Row)`
+  padding: 10px 0px;
+`;
